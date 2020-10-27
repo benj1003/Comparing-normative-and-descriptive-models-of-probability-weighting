f00263363ed04f547f49f611a8456d901850b242
benj1003 <54884346+benj1003@users.noreply.github.com>
2020-10-27 15:41:23 +0100

    Initial refignments towards this project

--- Parameter recovery/runHLM1.m
+++ Parameter recovery/runHLM1.m
@@ -3,28 +3,23 @@
 % It provides the following inputs when calling setHLM:
 
 % runModelNum - which models to run
-% synthMode   - which data to use: 1 real participants, 2 synthetic agents for model recovery, 3 synthetic agents for param recovery
+% mode        - simulating data or recovering parameters
 % whichJAGS   - which copy of matjags to run on. this allows parallel jobs to run as long as they use different matjags
 % whichQuals  - sets the order of qualities to run
-% runPlots    - plot results, illustrations, schematics
 
 % The idea is that this is written into by the user, then called by a
 % cluster job via the terminal:
-
-% runHLM1.m is called by runHLM_job1.sh
-% runHLM2.m is called by runHLM_job2.sh, and so on.
 
 %% Add to path
 cd .. ;%move to base directory 
 addpath(genpath(pwd));%adds base directory and subfolders to path, important for running shell scripts from terminal
 
 %% Specify variables
-runModelNum=6;
-synthMode=1;
+runModelNum=1;
+Mode=1;
 whichJAGS=1;
-whichQuals=3:5;
-runPlots=0;
+whichQuals=1;
 doParallel=0;
 
 %% Call setHLM
-setHLM(runModelNum,synthMode,whichJAGS,whichQuals,runPlots,doParallel)
+setHLM(runModelNum,Mode,whichJAGS,whichQuals,doParallel)
