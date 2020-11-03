cf549c797a075c4c40ebe8c3ed0d4567d3ec27d1
benj1003 <54884346+benj1003@users.noreply.github.com>
2020-11-03 14:16:53 +0100

    Deleting testoutputs

--- Parameter recovery/test.m
+++ Parameter recovery/test.m
@@ -1,5 +1,1 @@
-clear all;
-dataSource = sprintf('gamble_%d_all_sessions_permuted=%s',whichGamble,permuted);%All data needed to simulate choices for specific gamble
-load(dataSource)
-
-t
+size(samples.y_lml)
