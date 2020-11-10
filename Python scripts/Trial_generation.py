a4117ecfe74158a604eebf5b84355547f9018c0e
benj1003 <54884346+benj1003@users.noreply.github.com>
2020-11-10 13:52:27 +0100

    Including empty choice vec

--- Python scripts/Trial_generation.py
+++ Python scripts/Trial_generation.py
@@ -7,6 +7,7 @@ import sys
 
 def trial_generation(gamble_df, g, nTrial=100, threshold=0.01):
     gamble_nr = [(g+1)]*nTrial
+    choice = [np.nan]*nTrial
     a_max = [gambles_df["A1_x"][g]]*nTrial
     a_min = [gambles_df["A2_x"][g]]*nTrial
     b_max = [gambles_df["B1_x"][g]]*nTrial
@@ -46,16 +47,17 @@ def trial_generation(gamble_df, g, nTrial=100, threshold=0.01):
 
         if abs(prob_max_a[-1] - gambles_df["A1_p"][g]) < threshold and abs(prob_max_b[-1] - gambles_df["B1_p"][g]) < threshold:
             acceepted = True
-            data.append([gamble_nr,trial_count, a_max,a_min,choice_a,prob_max_a,b_max,b_min,choice_b,prob_max_b])
+            data.append([gamble_nr,trial_count,choice, a_max,a_min,choice_a,prob_max_a,b_max,b_min,choice_b,prob_max_b])
             df = pd.DataFrame(data =data[0]) 
             df = df.transpose()
-            df.columns = ['Gamble_nr','Trial_count','maxA','minA','xA', 'pMaxA','maxB','minB', 'xB', 'pMaxB']
+            df.columns = ['Gamble_nr','Trial_count','Choice','maxA','minA','xA', 'pMaxA','maxB','minB', 'xB', 'pMaxB']
 
             return df
 
 def duplicate_trials_to_sessions(data, nSessions):
     gamble_nr = list(data['Gamble_nr'])*nSessions
     trial_count = list(data['Trial_count'])*nSessions
+    choice = list(data['Choice'])*nSessions
     maxA = list(data['maxA'])*nSessions
     minA = list(data['minA'])*nSessions
     xA = list(data['xA'])*nSessions
@@ -65,14 +67,14 @@ def duplicate_trials_to_sessions(data, nSessions):
     xB = list(data['xB'])*nSessions
     pMaxB = list(data['pMaxB'])*nSessions
 
-    data = [gamble_nr,trial_count, maxA,minA,xA,pMaxA,maxB,minB,xB,pMaxB]
+    data = [gamble_nr,trial_count,choice, maxA,minA,xA,pMaxA,maxB,minB,xB,pMaxB]
     df = pd.DataFrame(data =data) 
     df = df.transpose()
-    df.columns = ['Gamble_nr','Trial_count','maxA','minA','xA', 'pMaxA','maxB','minB', 'xB', 'pMaxB']
+    df.columns = ['Gamble_nr','Trial_count','Choice','maxA','minA','xA', 'pMaxA','maxB','minB', 'xB', 'pMaxB']
 
     metadata = [gamble_nr[0], maxA[0], minA[0], maxB[0], nSessions, len(gamble_nr)/nSessions]
 
-    datadict = {'maxA':maxA, 'minA': minA,'xA': xA, 'p_maxA': pMaxA, 'maxB': maxB, 'minB': minB, 'xB': xB, 'p_maxB': pMaxB}
+    datadict = {'Choice':choice,'maxA':maxA, 'minA': minA,'xA': xA, 'p_maxA': pMaxA, 'maxB': maxB, 'minB': minB, 'xB': xB, 'p_maxB': pMaxB}
 
     return datadict, metadata
 
