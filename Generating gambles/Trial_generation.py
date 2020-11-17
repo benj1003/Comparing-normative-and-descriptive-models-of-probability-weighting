645b9a4e88e575a21f94f1f2ac88fd2c0ad63d81
benj1003 <54884346+benj1003@users.noreply.github.com>
2020-11-17 11:39:09 +0100

    New structure of gambles

--- Generating gambles/Trial_generation.py
+++ Generating gambles/Trial_generation.py
@@ -5,7 +5,7 @@ import random
 import matplotlib.pyplot as plt 
 import sys
 
-def trial_generation(gamble_df, g, nTrial=100, threshold=0.01):
+def trial_generation(gamble_df, g, nTrial, nAgent, threshold=1):
     gamble_nr = [(g+1)]*nTrial
     choice = [np.nan]*nTrial
     a_max = [gambles_df["A1_x"][g]]*nTrial
@@ -13,89 +13,65 @@ def trial_generation(gamble_df, g, nTrial=100, threshold=0.01):
     b_max = [gambles_df["B1_x"][g]]*nTrial
     b_min = [gambles_df["B2_x"][g]]*nTrial
 
-    accepted = False #bool check if frequency probabilities are sufficiently close to underlying probability
+    metadata = [gamble_nr[0], a_max[0], a_min[0], b_max[0], nAgent, nTrial]
 
-    while not accepted:
-        data = []
-        trial_count = []
-        choice_a = []      #outcome in trial for choice A
-        prob_max_a = []    #probability of a_max according to frequency 
-        choice_b = []      #outcome in trial for choice B
-        prob_max_b = []    #probability of b_max according to frequency 
-        
-        max_a_counter = 0
-        max_b_counter = 0
-        for ii in range(nTrial):
-            trial_count.append(ii)
+    data = []
+    for jj in range(nAgent):
+        accepted = False #bool check if frequency probabilities are sufficiently close to underlying probability
+        while not accepted:
+            data_for_agent = []
+            trial_count = []
+            choice_a = []      #outcome in trial for choice A
+            prob_max_a = []    #probability of a_max according to frequency 
+            choice_b = []      #outcome in trial for choice B
+            prob_max_b = []    #probability of b_max according to frequency 
+            
+            max_a_counter = 0
+            max_b_counter = 0
+            for ii in range(nTrial):
+                trial_count.append(ii)
 
-            #Choice A
-            rnd = np.random.uniform(0,1,1)
-            choice_a.append(gambles_df["A1_x"][g] if rnd < gambles_df["A1_p"][g] else gambles_df["A2_x"][g]) 
+                #Choice A
+                rnd = np.random.uniform(0,1,1)
+                choice_a.append(gambles_df["A1_x"][g] if rnd < gambles_df["A1_p"][g] else gambles_df["A2_x"][g]) 
 
-            if choice_a[ii] == gambles_df["A1_x"][g]:
-                max_a_counter += 1
-            p_a_tmp = max_a_counter/(ii+1)
-            prob_max_a.append(p_a_tmp)
+                if choice_a[ii] == gambles_df["A1_x"][g]:
+                    max_a_counter += 1
+                p_a_tmp = max_a_counter/(ii+1)
+                prob_max_a.append(p_a_tmp)
 
-            #Choice B
-            choice_b.append(gambles_df["B1_x"][g] if np.random.uniform(0,1,1) < gambles_df["B1_p"][g] else gambles_df["B2_x"][g])
+                #Choice B
+                choice_b.append(gambles_df["B1_x"][g] if np.random.uniform(0,1,1) < gambles_df["B1_p"][g] else gambles_df["B2_x"][g])
 
-            if choice_b[ii] == gambles_df["B1_x"][g]:
-                max_b_counter += 1
-            p_b_tmp = max_b_counter/(ii+1)
-            prob_max_b.append(p_b_tmp)
-
-        if abs(prob_max_a[-1] - gambles_df["A1_p"][g]) < threshold and abs(prob_max_b[-1] - gambles_df["B1_p"][g]) < threshold:
-            acceepted = True
-            data.append([gamble_nr,trial_count,choice, a_max,a_min,choice_a,prob_max_a,b_max,b_min,choice_b,prob_max_b])
-            df = pd.DataFrame(data =data[0]) 
-            df = df.transpose()
-            df.columns = ['Gamble_nr','Trial_count','Choice','maxA','minA','xA', 'pMaxA','maxB','minB', 'xB', 'pMaxB']
-
-            return df
-
-def duplicate_trials_to_sessions(data, nSessions):
-    gamble_nr = list(data['Gamble_nr'])*nSessions
-    trial_count = list(data['Trial_count'])*nSessions
-    choice = list(data['Choice'])*nSessions
-    maxA = list(data['maxA'])*nSessions
-    minA = list(data['minA'])*nSessions
-    xA = list(data['xA'])*nSessions
-    pMaxA = list(data['pMaxA'])*nSessions
-    maxB = list(data['maxB'])*nSessions
-    minB = list(data['minB'])*nSessions
-    xB = list(data['xB'])*nSessions
-    pMaxB = list(data['pMaxB'])*nSessions
-
-    data = [gamble_nr,trial_count,choice, maxA,minA,xA,pMaxA,maxB,minB,xB,pMaxB]
-    df = pd.DataFrame(data =data) 
-    df = df.transpose()
-    df.columns = ['Gamble_nr','Trial_count','Choice','maxA','minA','xA', 'pMaxA','maxB','minB', 'xB', 'pMaxB']
-
-    metadata = [gamble_nr[0], maxA[0], minA[0], maxB[0], nSessions, len(gamble_nr)/nSessions]
-
-    datadict = {'Choice':choice,'maxA':maxA, 'minA': minA,'xA': xA, 'p_maxA': pMaxA, 'maxB': maxB, 'minB': minB, 'xB': xB, 'p_maxB': pMaxB}
-
-    return datadict, metadata
+                if choice_b[ii] == gambles_df["B1_x"][g]:
+                    max_b_counter += 1
+                p_b_tmp = max_b_counter/(ii+1)
+                prob_max_b.append(p_b_tmp)
 
 
-#making sure the path is correct
-if not os.getcwd() == r'c:\Users\benja\OneDrive\Dokumenter\GitHub\Master-thesis\Python scripts':
-    os.chdir(r'c:\Users\benja\OneDrive\Dokumenter\GitHub\Master-thesis\Python scripts')
+            prob_a_within_threshold = abs(prob_max_a[-1] - gambles_df["A1_p"][g]) < threshold
+            prob_b_within_threshold = abs(prob_max_b[-1] - gambles_df["B1_p"][g]) < threshold
+
+
+            if prob_a_within_threshold and prob_b_within_threshold:
+                accepted = True
+                data_for_agent = {'Choice':choice,'maxA':a_max, 'minA': a_min,'xA': choice_a, 'p_maxA': prob_max_a, 'maxB': b_max, 'minB': b_min, 'xB': choice_b, 'p_maxB': prob_max_b}
+                data.append(data_for_agent)
+                
+    return data, metadata
 
 #Import gambles
-gambles_df = pd.read_csv('Gambles.csv', sep=";")
+gambles_df = pd.read_csv('Generating gambles\Gambles.csv', sep=";")
 nGambles = gambles_df.shape[0] 
 
-cols = ['Gamble_nr','maxA','minA','maxB','nTrials','nSessions']
-df_metadata = pd.DataFrame(columns=cols, index=range(nGambles))
-datadict_full = []
+cols = ['Gamble_nr','maxA','minA','maxB','nAgents','nTrials']
+df_metadata_all_gambles = pd.DataFrame(columns=cols, index=range(nGambles))
 
+data_all_gamles = []
 for g in range(nGambles):
-    data = trial_generation(gambles_df, g)
-    datadict, metadata = duplicate_trials_to_sessions(data, 50)
-    df_metadata.loc[g] = metadata
-    datadict_full.append(datadict)
+    data_one_gamble, metadata_one_gamble = trial_generation(gambles_df, g, 2, 2)
+    df_metadata_all_gambles.loc[g] = metadata_one_gamble
+    data_all_gamles.append(data_one_gamble)
 
-df_metadata.to_csv(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'Parameter recovery','data',f"metadata.txt")),index=False)
-scipy.io.savemat(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'Parameter recovery','data','all_gambles.mat')),datadict,oned_as='row')
+df_metadata_all_gambles.to_csv(os.path.abspath(os.path.join(os.path.dirname( __file__ ),'..', 'Recovery','data',f"metadata.txt")),index=False)
+scipy.io.savemat(os.path.abspath(os.path.join(os.path.dirname( __file__ ),'..', 'Recovery','data','all_gambles.mat')),{'datadict':data_all_gamles},oned_as='row') 
