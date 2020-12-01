6a85d3b2b4bae84341d679e61903dffbcab9a037
benj1003 <s182848@student.dtu.dk>
2020-12-01 13:44:04 +0100

    getting analysis files ready

--- Recovery/analysis/model_recovery.py
+++ Recovery/analysis/model_recovery.py
@@ -1,17 +1,40 @@
-from Read_output import read_output
+from functions import read_output
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-_,_,beta_cpt,beta_lml,delta,gamma = read_output('parameter_recovery_CPT_Gamble_1.mat','parameter_recovery')
-_,_,beta_cpt_true,beta_lml_true,delta_true,gamma_true = read_output('Choices_simulated_from_CPT_Gamble_1.mat','parameter_recovery')
+z = read_output('model_comparison_LML_Gamble_2.mat','model_comparisson')
 
-n_subjects = np.shape(beta_cpt)[0]
-n_samples = np.shape(beta_cpt)[1]
-n_trials = np.shape(beta_cpt)[2]
+n_subjects = np.shape(z)[0]
+n_samples = np.shape(z)[1]
+n_chains = np.shape(z)[2]
 
-print('---------------------------------------')
+print('---------------------------------------') 
 print(f"Number of subjects = {n_subjects}")
-print(f"Number of trials = {n_trials}")
+print(f"Number of chains = {n_chains}")
 print(f"Number of samples = {n_samples}")
 print('---------------------------------------')
+
+print(np.shape(z))
+
+lml = 0
+cpt = 0
+z_choices2 = []
+for i in range(n_subjects):
+    for j in range(n_chains):
+        for s in range(n_samples):
+            z_choices2.append(z[i,s,j])
+            if z[i,s,j] in [1,3,5,7]:
+                # print(z[i,s,j])
+                cpt += 1
+            else:
+                lml += 1
+z_choices = [cpt/(n_subjects*n_samples*n_chains),lml/(n_subjects*n_samples*n_chains)]
+print(z_choices)
+
+x = ['CPT','LML']
+plt.figure()
+plt.suptitle("Modelindicator")
+plt.bar(x,z_choices)
+plt.ylim([0,1])
+plt.show()
--- Recovery/analysis/parameter_recovery.py
+++ Recovery/analysis/parameter_recovery.py
@@ -1,20 +1,118 @@
-from Read_output import read_output
+from functions import read_output, process_params, cpt_weighting_function
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sys
 
-_,_,beta_cpt,beta_lml,delta,gamma = read_output('parameter_recovery_CPT_Gamble_1.mat','parameter_recovery')
+cpt_file_name = 'parameter_recovery_CPT_Gamble_1.mat'
+lml_file_name = "test"
+
+print("------------------------------------")
+print("Evaluating CPT data...\n")
+print("Reading output...")
+_,_,beta_cpt,beta_lml,delta,gamma = read_output(cpt_file_name,'parameter_recovery')
 _,_,beta_cpt_true,beta_lml_true,delta_true,gamma_true = read_output('Choices_simulated_from_CPT_Gamble_1.mat','parameter_recovery')
 
 n_chunks = np.shape(beta_cpt)[0]
 n_subjects = np.shape(beta_cpt)[1]
 n_samples = np.shape(beta_cpt)[2]
-n_trials = np.shape(beta_cpt)[3]
+n_chains = np.shape(beta_cpt)[3]
 
-print('---------------------------------------')
+print('---')
 print(f"Number of subjects = {n_subjects}")
 print(f"Number of chunks = {n_chunks}")
-print(f"Number of trials = {n_trials}")
+print(f"Number of chains = {n_chains}")
 print(f"Number of samples = {n_samples}")
-print('---------------------------------------')
+print('---')
 
+#--------delta----------#
+print("\nProcessing Delta...")
+map_marginal_delta, map_chunks_delta, map_subjects_delta = process_params(delta, n_chunks, n_subjects, n_chains, n_samples, output="map")
+
+plt.figure()
+for i in range(n_chunks):
+    plt.subplot(2,5,i+1)
+    plt.scatter(delta_true,map_subjects_delta[i])
+
+#--------gamma----------#
+print("\nProcessing Gamma...")
+map_marginal_gamma, map_chunks_gamma, map_subjects_gamma = process_params(gamma, n_chunks, n_subjects, n_chains, n_samples, output="map")
+
+plt.figure()
+for i in range(n_chunks):
+    plt.subplot(2,5,i+1)
+    plt.scatter(gamma_true,map_subjects_gamma[i])
+
+
+#--------w over chunks (delta,gamma)----------#
+print("\nProcessing w...")
+plt.figure()
+for i in range(n_chunks):
+    plt.suptitle("Weighting function parameters envolvment over time for CPT species")
+    plt.subplot(2,5,i+1)
+    plt.title(f"Chunk {i+1}")
+    plt.scatter(map_chunks_delta[i], map_chunks_gamma[i])
+    plt.xlim([0,1])
+    plt.ylim([0,1])
+
+plt.figure()
+x = np.linspace(0,1,100)
+for i in range(n_chunks):
+    w = cpt_weighting_function(x,map_chunks_gamma[i], map_chunks_delta[i])
+    w_true = cpt_weighting_function(x, np.median(gamma_true), np.median(delta_true))
+    
+    plt.subplot(2,5,i+1)
+    plt.plot(x,w, label="est")
+    plt.plot(x,w_true, label="True")
+    plt.legend()
+
+print("\nPlotting...")
+plt.show()
+
+print("------------------------------------")
+print("Evaluating LML data\n")
+print("Reading output...")
+_,_,beta_cpt,beta_lml,delta,gamma = read_output(lml_file_name,'parameter_recovery')
+n_chunks = np.shape(beta_cpt)[0]
+n_subjects = np.shape(beta_cpt)[1]
+n_samples = np.shape(beta_cpt)[2]
+n_chains = np.shape(beta_cpt)[3]
+
+print('---')
+print(f"Number of subjects = {n_subjects}")
+print(f"Number of chunks = {n_chunks}")
+print(f"Number of chains = {n_chains}")
+print(f"Number of samples = {n_samples}")
+print('---')
+
+#--------delta----------#
+print("\nProcessing Delta...")
+map_marginal_delta, map_chunks_delta, map_subjects_delta = process_params(delta, n_chunks, n_subjects, n_chains, n_samples, output="map")
+
+#--------gamma----------#
+print("\nProcessing Gamma...")
+map_marginal_gamma, map_chunks_gamma, map_subjects_gamma = process_params(gamma, n_chunks, n_subjects, n_chains, n_samples, output="map")
+
+#--------w over chunks (delta,gamma)----------#
+print("\nProcessing w...")
+plt.figure()
+for i in range(n_chunks):
+    plt.suptitle("Weighting function parameters envolvment over time for CPT species")
+    plt.subplot(2,5,i+1)
+    plt.title(f"Chunk {i+1}")
+    plt.scatter(map_chunks_delta[i], map_chunks_gamma[i])
+    plt.xlim([0,1])
+    plt.ylim([0,1])
+
+plt.figure()
+x = np.linspace(0,1,100)
+for i in range(n_chunks):
+    w = cpt_weighting_function(x,map_chunks_gamma[i], map_chunks_delta[i])
+    
+    plt.subplot(2,5,i+1)
+    plt.plot(x,w, label="est")
+    plt.plot(x,w_true, label="True")
+    plt.legend()
+
+print("\nPlotting...")
+plt.show()
