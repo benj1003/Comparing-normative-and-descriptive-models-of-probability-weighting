a578ee97bcccc902c3d06cead879ec0abad45089
benj1003 <s182848@student.dtu.dk>
2020-12-09 17:56:58 +0100

    Init (not usable)

--- Recovery/analysis/model_recovery.py
+++ Recovery/analysis/model_recovery.py
@@ -4,39 +4,39 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import sys
 
-z_lml = read_output('model_comparison_LML_Gamble_2.mat','model_comparisson')
+#z_lml = read_output('model_comparison_CPT_Gamble_2.mat','model_comparisson')
 z_cpt = read_output('model_comparison_CPT_Gamble_2.mat','model_comparisson')
 
-print(z_lml)
-print()
-print(z_cpt)
+#print(z_lml)
+#print()
+#print(z_cpt)
 
-sys.exit()
-n_subjects = np.shape(z_lml)[0]
-n_samples = np.shape(z_lml)[1]
-n_chains = np.shape(z_lml)[2]
+#sys.exit()
+n_subjects = np.shape(z_cpt)[0]
+n_samples = np.shape(z_cpt)[1]
+n_chains = np.shape(z_cpt)[2]
 
 print('---------------------------------------') 
 print(f"Number of subjects = {n_subjects}")
 print(f"Number of chains = {n_chains}")
 print(f"Number of samples = {n_samples}")
 print('---------------------------------------')
-
+#sys.exit()
 #Running on lml_data
-z_choices_lml, z_choices_subject_lml = model_select(z_lml, n_subjects, n_chains, n_samples)
+#z_choices_lml, z_choices_subject_lml = model_select(z_lml, n_subjects, n_chains, n_samples)
 z_choices_cpt, z_choices_subject_cpt = model_select(z_cpt, n_subjects, n_chains, n_samples)
 
 
-print(z_choices_lml)
+#print(z_choices_lml)
 #Running on lml_data
 print(z_choices_cpt)
-
+#sys.exit()
 #Raw plot
-labels = ['CPT-LML','LML-LML']
-plt.figure()
-plt.suptitle("Modelindicator")
-plt.bar(labels,z_choices_lml)
-plt.ylim([0,1])
+# labels = ['CPT-LML','LML-LML']
+# plt.figure()
+# plt.suptitle("Modelindicator")
+# plt.bar(labels,z_choices_lml)
+# plt.ylim([0,1])
 
 labels = ['CPT-CPT','LML-CPT']
 plt.figure()
@@ -46,12 +46,12 @@ plt.ylim([0,1])
 
 plt.show()
 
-intensities = z_choices_subject_lml + z_choices_subject_cpt
+intensities = z_choices_subject_cpt #+ z_choices_subject_cpt
 #Heatmap
 labels = ['CPT','LML']
 cmap = plt.get_cmap('binary')
 fig, ax = plt.subplots()
-plot = ax.pcolormesh(range(len(labels)+1), range(100), intensities, cmap=cmap)
+plot = ax.pcolormesh(range(len(labels)+1), range(50), intensities, cmap=cmap)
 ax.set_yticks([1,25,50,51,75,100])
 ax.set_yticklabels(['1','25','50','1','25','50'],fontsize = 10)
 ax.set_xticks(np.arange(len(labels))+0.5)
