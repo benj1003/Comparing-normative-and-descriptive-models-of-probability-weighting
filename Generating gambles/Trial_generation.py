69adc9e56fc0c188c818f735ad607f20337e8ff4
benj1003 <s182848@student.dtu.dk>
2020-12-15 10:06:48 +0100

    Setting up final run with 10 agents and 200 trials

--- Generating gambles/Trial_generation.py
+++ Generating gambles/Trial_generation.py
@@ -71,7 +71,7 @@ df_metadata_all_gambles = pd.DataFrame(columns=cols, index=range(nGambles))
 #Generate trials
 data_all_gamles = []
 for g in range(nGambles):
-    print(f"Completed Gamble {g+1}"")
+    print(f"Completed Gamble {g+1}")
     data_one_gamble, metadata_one_gamble = trial_generation(gambles_df, g, nTrial=200, nAgent=10)
     df_metadata_all_gambles.loc[g] = metadata_one_gamble
     data_all_gamles.append(data_one_gamble)
